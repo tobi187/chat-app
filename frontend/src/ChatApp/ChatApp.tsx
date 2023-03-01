8ec1f270ef292fedbc42026737fa7a75375563d2
Tobias Fischer <61592216+tobi187@users.noreply.github.com>
2023-03-01 13:37:36 +0100

    Update ChatApp.tsx

--- frontend/src/ChatApp/ChatApp.tsx
+++ frontend/src/ChatApp/ChatApp.tsx
@@ -1,51 +1,111 @@
 import "./ChatApp.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
+import { type } from "os";
 
-function OwnMessages() {
+function OwnMessages(mess: Message) {
   return (
     <div className="Messages">
       <div className="d-flex justify-content-end">
         <div className="card">
-          <div className="card-body">Some of my own text</div>
+          <div className="card-body">{mess.message}</div>
         </div>
       </div>
     </div>
   );
 }
 
-function OtherMessages() {
+function OtherMessages(mess: Message) {
   return (
     <div>
       <div className="d-flex justify-content-start">
         <div className="card">
-          <div className="card-body">Some Other Text</div>
+          <span>{mess.name}</span>
+          <div className="card-body">{mess.message}</div>
         </div>
       </div>
     </div>
   );
 }
+
+type Message = {
+  name: string,
+  message: string
+}
+
+const spawnMessages = (mess: Message[]) => {
+  const el = document.getElementById("mess-con")
+  
+  const userName = (document.getElementById("user-name") as HTMLInputElement).value
+  const arr = mess.map(m => {
+    const outer = document.createElement("div")
+    const c = document.createElement("div")
+    c.classList.add("card")
+    const cb = document.createElement("div")
+    cb.classList.add("card-body")
+    cb.innerText = m.message
+    
+    if (m.name == userName) {
+      outer.classList.add("d-flex")
+      outer.classList.add("justify-content-end")
+    } else {
+      outer.classList.add("d-flex")
+      outer.classList.add("justify-content-start")
+      const s = document.createElement("span")
+      s.innerText = m.name
+      c.appendChild(s)
+    }
+    c.appendChild(cb)
+    outer.appendChild(c)
+    return outer
+  });
+
+  el?.replaceChildren(...arr)
+}
+
+
+
+const sendMessage = async () =>  {
+  const message = document.getElementById("message-text") as HTMLInputElement
+  const userName = document.getElementById("user-name") as HTMLInputElement
+  const result = await fetch("http://localhost:5000/message", {
+      // Add parameters here
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*' 
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        name: userName.value,
+        message: message.value
+      })
+    })
+  
+  const messages = await result.json() as Message[]
+
+  spawnMessages(messages)
+}
+
+setInterval(async () => {
+  console.log("running")
+  const res = await fetch("http://localhost:5000/all", {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*' 
+    },
+    method: 'GET'
+  })
+  const data = await res.json() as Message[]
+  spawnMessages(data)
+}, 3000)
+
+
+
 export default function ChatApp() {
   // Using Axios - ensure you first install the package
-  axios
-    .post("http://localhost:5000/Chat", {
-      // Add parameters here
-      name: "tobu",
-      message:"Hi"
-    })
-    .then((response) => {
-      console.log(response.data);
-      // Handle data
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-  fetch("http://localhost:5000/startpage")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-
+ 
   return (
     <div>
       <h1 className="title">Chat App</h1>
@@ -63,32 +123,34 @@ export default function ChatApp() {
                 placeholder="Username"
                 aria-label="Username"
                 aria-describedby="basic-addon1"
+                id="user-name"
               />
             </div>
           </form>
         </nav>
       </div>
 
-      <div className="container">
-        <div className="own Messages">{OwnMessages()}</div>
-        <div className="other Messages">{OtherMessages()}</div>
+      <div className="container mess-con" id="mess-con">
+        
       </div>
 
       <div className="inputMessage">
-        <form className="input-group mb-3 my-form" method="POST">
+        <div className="input-group mb-3 my-form">
           <input
             type="text"
             className="form-control"
             placeholder="Neue Nachricht schreiben"
+            id="message-text"
           />
           <button
             className="btn btn-outline-secondary"
             type="submit"
             id="button-addon2"
+            onClick={sendMessage}
           >
             Senden
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
